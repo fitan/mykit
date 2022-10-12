c094c757c8fc79aefb498985a641e94318689140
安博微 <boweian@creditease.cn>
2022-10-12 17:37:57 +0800

    add

--- mytemplate/app/init.go
+++ mytemplate/app/init.go
@@ -13,7 +13,11 @@ import (
 	"go.uber.org/zap/zapcore"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	gormLog "log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func initConf() (*conf.Conf, error) {
@@ -28,7 +32,20 @@ func initConf() (*conf.Conf, error) {
 func initGORM(conf *conf.Conf, log *zap.SugaredLogger) (*mygorm.DB, error) {
 	log.Infow("init gorm staring...")
 	defer log.Infow("init gorm end...")
-	db, err := gorm.Open(mysql.Open(conf.Mysql.DSN))
+
+	newLogger := logger.New(
+		gormLog.New(os.Stdout, "\r\n", gormLog.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second,   // 慢 SQL 阈值
+			LogLevel:                  logger.Silent, // 日志级别
+			IgnoreRecordNotFoundError: true,          // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  false,         // 禁用彩色打印
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(conf.Mysql.DSN), &gorm.Config{
+		Logger: newLogger,
+	})
 	return mygorm.New(db), err
 }
 
